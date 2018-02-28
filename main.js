10634f0a84aa20022a868b8ad980e6f5f5d5513f
Zoe Ames <zoeames@gmail.com>
2018-02-27 19:46:26 -0600

    finished first exercise

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
 console.log("hi");
+
+
+var a = [1, 2, 3];
+a.join(' ');  //"1 2 3"
+
+
+//challenge 1
+
+var test = "cat";
+
+// ["c", "a", "t"]  = > ["t", "a", "c"]
+var opp = test.split("").reverse().join("");
+
+if(test === opp){
+  console.log("same? ", true);
+}else{
+  console.log("same? ", false);
+}
