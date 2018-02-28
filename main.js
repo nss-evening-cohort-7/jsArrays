546d501eb10e7783eac9236442b49112faf14645
Zoe Ames <zoeames@gmail.com>
2018-02-27 21:18:13 -0600

    added challenge 2

--- main.js
+++ main.js
@@ -17,3 +17,30 @@ if(test === opp){
 }else{
   console.log("same? ", false);
 }
+
+
+
+
+var animals = ['fish', 'dog', 'cat'];
+var animalzElement = document.getElementById('animalz');
+
+for(var i=0; i<animals.length; i++){
+  // console.log("animal", animals[i]);
+  animalzElement.innerHTML += '<h5>' + animals[i] + '</h5>';
+}
+
+
+// Challenge 2
+
+var book = "catch 202";
+var bookSplit = book.split("");
+var realNumz = [];
+for(var j=0; j<bookSplit.length; j++){
+  // console.log(bookSplit[j]*1);
+  if(bookSplit[j]*1){
+    realNumz.push(bookSplit[j]);
+  }else if(bookSplit[j] === '0'){
+    realNumz.push(bookSplit[j]);
+  }
+}
+console.log("answer", realNumz.join(""))
